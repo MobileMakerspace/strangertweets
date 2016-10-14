08c6a6d23bf17dac9603c1034fdb7887fae1fa8f
Dana Spisak <dana_spisak@mentor.com>
2016-10-14 22:14:13 +0000

    Ready to go!

--- strangertweets.py
+++ strangertweets.py
@@ -1,3 +1,4 @@
+import random
 import time
 import re
 import os
@@ -16,14 +17,24 @@ logging.basicConfig(level=logging.DEBUG,
 load_dotenv(find_dotenv())
 
 # Search terms
-TERMS = '#KenBoneFacts'
+TERMS = '#spookywalk'
 
 
 # number of LEDS
-numLEDS = 26
+numLEDS = 50
+
+RED = (0,255,0)
+GREEN = (255,0,0)
+BLUE = (0,0,255)
 
 client = opc.Client('localhost:7890')
 alpha = [(0,0,0)] * numLEDS
+all_on = [(255,255,255)] * numLEDS
+all_off = [(0,0,0)] * numLEDS
+all_red = [RED] * numLEDS
+init_strand = [(0,0,0)] * numLEDS
+m = [(0,0,0)]*numLEDS
+mapping = [5,7,8,10,12,13,14,16,33,31,30,28,27,26,23,22,20,39,40,41,43,45,46,47,48,50]
 
 # Twitter application authentication
 APP_KEY = os.environ['APP_KEY']
@@ -33,7 +44,7 @@ OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
 
 
 
-q = Queue.Queue(20)
+q = Queue.Queue(40)
 
 
 
@@ -89,25 +100,51 @@ class ConsumerThread(threading.Thread):
         item = item.lower()
 
         # check for obscenity
+        item.replace('spookywalk','')
 
         # replace non-alpha
         item = re.sub('[^a-z]+', '', item)
         return item
 
+    def newTweet(self):
+        for x in xrange(3):
+            client.put_pixels(all_off)
+            time.sleep(0.5)
+            client.put_pixels(all_red)
+            time.sleep(0.05)
+            client.put_pixels(all_off)
+        time.sleep(1)
+
+    def lightShow(self):
+        for g in xrange(255):
+            pixel = random.randint(0,numLEDS-1)
+            m[pixel] = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+            client.put_pixels(m)
+            time.sleep(0.015)
+
+    def setupStrand(self):
+       for x in xrange(numLEDS):
+            init_strand[x] = (random.randint(0,255),random.randint(0,255),random.randint(0,255))            
+
     def run(self):
+        self.setupStrand()
         while True:
+            self.lightShow() 
             if not q.empty():
+                self.newTweet()
                 item = q.get()
                 logging.debug('Getting ' + str(item) 
                               + ' : ' + str(q.qsize()) + ' items in queue')
                 # loop thru the string in item putting the pixels
                 clean = self.cleanString(item)
+                print clean
                 for c in clean:
-                    pixel = ord(c) - ord('a')
-                    print pixel
-                    alpha[pixel] = (255,255,255)
+                    letter = ord(c) - ord('a')
+                    #print pixel
+                    pixel = mapping[letter] - 1
+                    alpha[pixel] = init_strand[pixel]
                     client.put_pixels(alpha)
-                    time.sleep(0.05)
+                    time.sleep(2)
                     alpha[pixel] = (0,0,0)
                     client.put_pixels(alpha)
                          
